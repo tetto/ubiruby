3d8b06521ed7a58921821d39b19073124423219b
Seva Rybakov <coding.fu@gmail.com>
2013-05-21 19:33:39 +0300

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -1,29 +1,29 @@
 #!/usr/bin/env ruby
 
-arg = ARGV[0]
-chars = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM".split('') , "eyuioaEYUIOA".split('')
-nums = "1234567890".split('')
-pass = ""
-init = rand(2)
-if init == 0
- pass = chars[0].sample
-else
- pass = chars[1].sample
-end
-if arg.to_i - 2 > 0
- (arg.to_i - 3).times {
-  if init == 0
-  pass += chars[1].sample
-  init = 1
-  else
-  pass += chars[0].sample
-  init = 0
+def generate(n)
+  chars = [
+    "qwrtpsdfghjklzxcvbnm",
+    "eyuioa"
+  ].map { |str| (str + str.upcase).split('') }
+  
+  delta = rand(2) # delta decides whether we start from alphabet 1 or two
+  pass = ""
+  # n - 2 alphanumeric
+  (n-2).times do |index|
+    alphabet_index = (index + delta) % 2
+    alphabet = chars[alphabet_index]
+    pass += alphabet.sample
   end
- }
- 2.times {pass += nums.sample}
- puts pass
-elsif arg == "--version"
- puts "v.0.1 by Tetto Green" 
+  # 2 digits
+  2.times { pass += rand(10) }
+  
+  puts pass
+end
+
+if arg == "--version"
+ puts "v.0.2 by Tetto Green"
+else if arg.to_i > 2
+ generate arg.to_i
 else
  puts "\nUsage: \n ./generator <n> - generate password with length of <n> (<n> > 2)"
 end
